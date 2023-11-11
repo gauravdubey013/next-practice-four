a98d34c0f1273facc1a6c8ec17d07327b478f190
Nilesh Dubey <nileshdubey@Nileshs-MacBook-Pro.local>
2023-11-11 07:04:47 +0530

    nextjs 8th practice blog under dev

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('648ad98bbedd6ce35a67');
+
+export default function Home() {
+  return (
+    <>
+    
+    </>
+  )
+}
